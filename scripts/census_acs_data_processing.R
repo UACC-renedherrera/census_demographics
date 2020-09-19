f25ee7008d46d9ba00116b30b19abf91f92dc5dd
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-09-18 17:46:42 -0700

    median age

--- scripts/census_acs_data_processing.R
+++ scripts/census_acs_data_processing.R
@@ -1,17 +1,52 @@
 # setup ----
-# load packages 
+# load packages
 library(here)
 library(tidyverse)
-library(stringr)
 library(knitr)
 
-# load data race ----
-# for catchment counties 
-race <- read_rds("data/tidy/acs5_2018_race.rds")
+# table format should be
+# | variable | geography | race | sex | year | estimate | table | source |
 
-# what is the total population
-# for each county and the total altogether
-race %>% 
-  group_by(NAME) %>%
-  filter(variable == "B02001_001") %>%
-  summarize()
+# median age
+# age 65 and over
+# total population
+# rural
+# female
+# race non hispanic white
+# race hispanic
+# race american indian
+# race non hispanic black
+
+#
+
+median_age <- read_rds("data/tidy/acs5_median_age.rds")
+
+# function to select areas to include on slides and visualizations
+select_areas_for_slide <- function(x){
+  areas <- c(
+    "United States",
+    "Arizona",
+    "Cochise",
+    "Pima",
+    "Pinal",
+    "Santa Cruz",
+    "Yuma"
+  )
+  x %>%
+    filter(NAME %in% areas)
+}
+
+# function to build the values for the slide
+build_table_for_slide <- function(x){
+  x %>%
+    filter(race == "all") %>%
+    select(NAME, variable, estimate) %>%
+    spread(key = NAME,
+           value = estimate) %>%
+    select(variable, `United States`, Arizona) %>%
+    mutate(Catchment = "34.3-40.6")
+}
+
+median_age %>% 
+  select_areas_for_slide %>%
+  build_table_for_slide()
