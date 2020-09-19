f77ff52a13552259433b91f97e3ca84b2218843b
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-09-18 17:46:36 -0700

    updates to median age

--- scripts/census_acs_data_tidy.R
+++ scripts/census_acs_data_tidy.R
@@ -2,11 +2,7 @@
 library(here)
 library(tidycensus)
 library(tidyverse)
-library(stringr)
-library(knitr)
-library(purrr)
-library(dataMaid)
-
+# library(dataMaid)
 # library(tigris)
 
 # for reference
@@ -17,17 +13,172 @@ var_acs_2018_acs5_profile <- load_variables(2018, "acs5/profile", cache = TRUE)
 var_acs_2018_acs5_subject <- load_variables(2018, "acs5/subject", cache = TRUE)
 var_acs_2018_acs1 <- load_variables(2018, "acs1", cache = TRUE)
 
-
 # write_csv(var_acs_2018_acs5, "data/tidy/ACS5_2018_variables.csv")
 
-# save catchment counties to value
-counties <- c(
-  "Cochise",
-  "Pima",
-  "Pinal",
-  "Santa Cruz",
-  "Yuma"
+# function that removes clutter from each county name
+# for example change from Apache County, Arizona to Apache
+trim_county_name <- function(x ){
+  x %>%
+    mutate(NAME = str_replace(x$NAME, " County, Arizona", ""))
+}
+
+# median age
+# median age usa ----
+
+acs5_median_age_usa <- get_acs(
+  geography = "us",
+  variables = c("median_age" = "B01002_001"),
+  cache_table = TRUE,
+  year = 2018,
+  survey = "acs5"
 )
+
+acs5_median_age_usa
+
+# median age az ----
+
+acs5_median_age_az <- get_acs(
+  geography = "state",
+  state = "az",
+  variables = c("median_age" = "B01002_001"),
+  cache_table = TRUE,
+  year = 2018,
+  survey = "acs5"
+)
+
+acs5_median_age_az
+
+# median age catchment ----
+# all az counties 
+
+acs5_median_age_az_county <- get_acs(
+  geography = "county",
+  variables = c(
+    "median_age" = "B01002_001"
+  ),
+  cache_table = TRUE,
+  year = 2018,
+  state = "az",
+  survey = "acs5"
+)
+
+acs5_median_age_az_county <- acs5_median_age_az_county %>%
+  trim_county_name()
+
+acs5_median_age_az_county
+
+# median age data table
+table_median_age <- bind_rows(acs5_median_age_usa, 
+          acs5_median_age_az,
+          acs5_median_age_az_county) %>%
+  #select(GEOID, NAME, variable, estimate) %>%
+  mutate(variable = "median age",
+    race = "all",
+         sex = "all",
+         year = "2014-2018",
+         source = "US Census: ACS5")
+
+table_median_age
+
+# catchment counties only 
+subset_catchment <- function(x){
+  counties <- c(
+    "Cochise",
+    "Pima",
+    "Pinal",
+    "Santa Cruz",
+    "Yuma"
+  )
+  x %>%
+    filter(x$NAME %in% counties)
+}
+
+acs5_median_age_az_county %>%
+  subset_catchment()
+
+# range of catchment counties only 
+acs5_median_age_az_county %>%
+  subset_catchment %>%
+  summarise(min(estimate),
+            max(estimate))
+
+# Median age catchment race ----
+# MEDIAN AGE BY SEX (HISPANIC OR LATINO)
+# Survey/Program: American Community Survey
+# Universe: People who are Hispanic or Latino
+# Year: 2018
+# Estimates: 5-Year
+# Table ID: B01002I
+#
+# Source: U.S. Census Bureau, 2014-2018 American Community Survey 5-Year Estimates
+
+median_age <- get_acs(
+  geography = "county",
+  state = "az",
+  variables = c("median_age_white" = "B01002H_001",
+                "median_age_hisp" = "B01002I_001",
+                "median_age_ai" = "B01002C_001",
+                "median_age_black" = "B01002B_001"),
+  cache_table = TRUE,
+  year = 2018,
+  survey = "acs5"
+)
+
+median_age <- median_age %>% trim_county_name()
+
+glimpse(median_age)
+
+# function to generate range of median age values for each race
+get_range_of_median_age <- function(x, race){
+  x %>%
+    filter(variable == race) %>%
+    summarise(min(estimate),
+              max(estimate))
+}
+
+# median age for WHITE ALONE, NOT HISPANIC OR LATINO in catchment
+median_age %>%
+  subset_catchment() %>%
+  get_range_of_median_age("median_age_white")
+
+# median age for BLACK OR AFRICAN AMERICAN ALONE in catchment
+median_age %>%
+  subset_catchment() %>%
+  get_range_of_median_age("median_age_black")
+
+# median age for HISPANIC OR LATINO in catchment
+median_age %>%
+  subset_catchment() %>%
+  get_range_of_median_age("median_age_hisp")
+
+# median age for AMERICAN INDIAN AND ALASKA NATIVE ALONE in catchment
+median_age %>%
+  subset_catchment() %>%
+  get_range_of_median_age("median_age_ai")
+
+# assemble table for median age including races
+median_age <- median_age %>%
+  mutate(race = case_when(
+    variable == "median_age_white" ~ "WHITE ALONE, NOT HISPANIC OR LATINO",
+    variable == "median_age_ai" ~ "AMERICAN INDIAN AND ALASKA NATIVE ALONE",
+    variable == "median_age_hisp" ~ "HISPANIC OR LATINO",
+    variable == "median_age_black" ~ "BLACK OR AFRICAN AMERICAN ALONE",
+    TRUE ~ "NA"
+  ),
+  sex = "all",
+  year = "2014-2018",
+  source = "US Census: ACS5",
+  variable = "median age")
+
+glimpse(median_age)
+
+# assemble table for median age for all known values 
+table_median_age <- bind_rows(table_median_age, median_age)
+glimpse(table_median_age)
+
+# save 
+write_rds(table_median_age, "data/tidy/acs5_median_age.rds")
+
 
 # load tables ----
 # AGE AND SEX
@@ -2355,127 +2506,7 @@ acs5_foreign_born_catch_hisp %>%
             total = sum(total)) %>%
   mutate(prop = foreign_born / total)
 
-# median age usa ----
 
-acs5_median_age_usa <- get_acs(
-  geography = "us",
-  variables = c("median_age_total" = "B01002_001"),
-  cache_table = TRUE,
-  year = 2018,
-  survey = "acs5"
-)
-
-acs5_median_age_usa
-
-# median age az ----
-
-acs5_median_age_az <- get_acs(
-  geography = "state",
-  state = "az",
-  variables = c("median_age_total" = "B01002_001"),
-  cache_table = TRUE,
-  year = 2018,
-  survey = "acs5"
-)
-
-acs5_median_age_az
-
-# median age catchment ----
-
-acs5_median_age_catchment <- get_acs(
-  geography = "county",
-  variables = c(
-    "median_age" = "B01002_001"
-  ),
-  cache_table = TRUE,
-  year = 2018,
-  state = "az",
-  survey = "acs5"
-)
-
-counties <- c(
-  "Cochise",
-  "Pima",
-  "Pinal",
-  "Santa Cruz",
-  "Yuma"
-)
-
-acs5_median_age_catchment %>%
-  mutate(NAME = str_replace(acs5_median_age_catchment$NAME, " County, Arizona", "")) %>%
-  filter(NAME %in% counties)
-
-acs5_median_age_catchment %>%
-  mutate(NAME = str_replace(acs5_median_age_catchment$NAME, " County, Arizona", "")) %>%
-  filter(NAME %in% counties) %>%
-  summarise(min(estimate),
-            max(estimate))
-
-# Median age catchment race ----
-# MEDIAN AGE BY SEX (HISPANIC OR LATINO)
-# Survey/Program: American Community Survey
-# Universe: People who are Hispanic or Latino
-# Year: 2018
-# Estimates: 5-Year
-# Table ID: B01002I
-#
-# Source: U.S. Census Bureau, 2014-2018 American Community Survey 5-Year Estimates
-
-median_age <- get_acs(
-  geography = "county",
-  state = "az",
-  variables = c("median_age_white" = "B01002A_001",
-                "median_age_hisp" = "B01002I_001",
-                "median_age_ai" = "B01002C_001",
-                "median_age_black" = "B01002B_001"),
-  cache_table = TRUE,
-  year = 2018,
-  survey = "acs5"
-)
-
-counties <- c(
-  "Cochise",
-  "Pima",
-  "Pinal",
-  "Santa Cruz",
-  "Yuma"
-)
-
-median_age %>%
-  mutate(NAME = str_replace(median_age$NAME, " County, Arizona", "")) %>%
-  filter(NAME %in% counties) %>%
-  select(!(moe)) %>%
-  spread(key = variable,
-         value = estimate) %>%
-  summarise(min(median_age_white),
-            max(median_age_white))
-
-median_age %>%
-  mutate(NAME = str_replace(median_age$NAME, " County, Arizona", "")) %>%
-  filter(NAME %in% counties) %>%
-  select(!(moe)) %>%
-  spread(key = variable,
-         value = estimate) %>%
-  summarise(min(median_age_black),
-            max(median_age_black))
-
-median_age %>%
-  mutate(NAME = str_replace(median_age$NAME, " County, Arizona", "")) %>%
-  filter(NAME %in% counties) %>%
-  select(!(moe)) %>%
-  spread(key = variable,
-         value = estimate) %>%
-  summarise(min(median_age_hisp),
-            max(median_age_hisp))
-
-median_age %>%
-  mutate(NAME = str_replace(median_age$NAME, " County, Arizona", "")) %>%
-  filter(NAME %in% counties) %>%
-  select(!(moe)) %>%
-  spread(key = variable,
-         value = estimate) %>%
-  summarise(min(median_age_ai),
-            max(median_age_ai))
 
 # language other than english usa ----
 # from table S1601
