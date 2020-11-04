04b9fcd2c42574a3a0f91db9a8126a1c29359999
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-11-04 14:59:38 -0700

    outline

--- scripts/census_acs_data_processing_for_uazcc.R
+++ scripts/census_acs_data_processing_for_uazcc.R
@@ -19,3 +19,54 @@ acs5_age_USA <- acs5_age_USA %>%
 acs5_age_USA %>%
   mutate(prop_65 = round(age_group_65 / Total, digits = 3),
          prop_18 = round(under_18 / Total, digits = 3))
+
+
+# sex ----
+# female
+# male
+
+
+
+
+# age ----
+# age under 18
+# age 18-39
+# age 65 and over
+
+
+
+
+# race and ethnicity ----
+# american indian / alaska native
+# asian pacific islander 
+# black
+# hispanic
+# non hispanic white
+
+
+
+# education ----
+# less than hs
+# hs
+# college
+
+
+
+# income ----
+# median family income
+# median household income
+
+
+# insurance ----
+# percent uninsured <65 year age
+
+
+# language ----
+
+
+
+# poverty ----
+
+
+
+# unemployed ----
